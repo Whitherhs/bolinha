ac18f7e5c873437368d984576bbf7690a1a69934
lnrdhffmnn <90478165+lnrdhffmnn@users.noreply.github.com>
2021-11-23 20:33:26 -0300

    Integração do menu com o jogo

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from graphics import *
-import random
-import time
 
-import eventos
-import blocos
+import menu
+import base
 import jogador
 
 # Cores
@@ -21,202 +19,17 @@ centro_tela = Point(
 )
 
 def main():
-    nome = jogador.tela_nome(janela, centro_tela, cor_principal, cor_secundaria)
-    jogo(nome)
-
-def jogo(nome):
-    # Variáveis de configuração da bolinha
-    # X = horizontal, Y = vertical
-    raio_bolinha = 15
-
-    pos_inicial_bolinha = Point(
-        centro_tela.getX(),
-        centro_tela.getY() / 3 * 4
-    )
-
-    velocidade_bolinha_x = 3
-    velocidade_bolinha_y = 4
-
-    sentido_bolinha_x = 0
-    sentido_bolinha_y = 1
-
-    # Variáveis de configuração do jogador (barra)
-    comprimento_jogador = 150
-    largura_jogador = 20
-
-    pos_inicial_jogador = Point(
-        (janela.getWidth() - comprimento_jogador) / 2,
-        janela.getHeight() - largura_jogador
-    )
-
-    velocidade_jogador = 30
-
-    # Variáveis de configuração do jogo
-    velocidade_jogo = 60
-    pontos = 0
-    altura_barra_superior = 30
-
-    # Bolinha
-    bolinha = Circle(pos_inicial_bolinha, raio_bolinha)
-    bolinha.setFill(cor_principal)
-    bolinha.draw(janela)
-
-    # Jogador
-    jogador = Line(pos_inicial_jogador, Point(
-        pos_inicial_jogador.getX() + comprimento_jogador,
-        pos_inicial_jogador.getY())
-    )
-    jogador.setFill(cor_principal)
-    jogador.setWidth(largura_jogador)
-    jogador.draw(janela)
-
-    # Barra superior
-    barra_superior = Line(
-        Point(0, altura_barra_superior),
-        Point(janela.getWidth(), altura_barra_superior)
-    )
-    barra_superior.setFill(cor_principal)
-    barra_superior.draw(janela)
-
-    # Texto na tela
-    texto_principal = Text(centro_tela,
-        'Pressione qualquer tecla para iniciar.'
-    )
-    texto_principal.setTextColor(cor_principal)
-    texto_principal.setSize(24)
-    texto_principal.draw(janela)
-
-    texto_nome = Text(
-        Point(30, 15),
-        nome.upper()
-    )
-    texto_nome.setTextColor(cor_principal)
-    texto_nome.setSize(12)
-    texto_nome.draw(janela)
-
-    texto_pontos = Text(
-        Point(centro_tela.getX(), 15),
-        f'Pontos: {pontos}'
-    )
-    texto_pontos.setTextColor(cor_principal)
-    texto_pontos.setSize(12)
-    texto_pontos.draw(janela)
-
-    texto_dificuldade = Text(
-        Point(janela.getWidth() - 30, 15),
-        'Fácil'
-    )
-    texto_dificuldade.setTextColor(cor_principal)
-    texto_dificuldade.setSize(12)
-    texto_dificuldade.draw(janela)
-
-    # Blocos
-    blocos_lista = blocos.desenhar_blocos(janela, cor_principal)
-
-    # Jogo foi aberto
-    eventos.jogo_abriu(janela)
-
-    # Loop principal
-    janela.getKey()
-
-    for i in range(3, 0, -1):
-        texto_principal.setText(i)
-        time.sleep(1)
-    texto_principal.setText('BOM JOGO!')
-    time.sleep(1)
-
-    texto_principal.undraw()
-
-    while True:
-        # Variáveis do loop
-        pos_atual_bolinha = bolinha.getCenter()
-
-        pos_atual_jogador = jogador.getCenter()
-
-        lado_esquerdo_jogador = pos_atual_jogador.getX() - (comprimento_jogador / 2)
-        lado_direito_jogador = pos_atual_jogador.getX() + (comprimento_jogador / 2)
-
-        colisao_com_jogador_x = lado_esquerdo_jogador <= pos_atual_bolinha.getX() <= lado_direito_jogador
-        colisao_com_jogador_y = pos_atual_bolinha.getY() >= pos_atual_jogador.getY()
-
-        # Movimento da bolinha
-        if pos_atual_bolinha.getX() <= 0 or pos_atual_bolinha.getX() >= janela.getWidth():
-            # Bolinha bateu no lado direito ou esquerdo da tela
-            sentido_bolinha_x *= -1
-
-        if pos_atual_bolinha.getY() <= altura_barra_superior or pos_atual_bolinha.getY() >= pos_atual_jogador.getY():
-            # Bolinha bateu no lado inferior ou superior da tela
-            if colisao_com_jogador_x and colisao_com_jogador_y:
-                # Colisão entre bolinha e jogador
-                velocidade_bolinha_y = 4 + (random.randrange(1, pontos + 2) / 10)
-                velocidade_bolinha_x = 3 + (random.randrange(1, pontos + 2) / 10)
-                if sentido_bolinha_x == 0:
-                    sentido_bolinha_x = random.choice((-1, 1))
-                eventos.colisao_bola_jogador()
-
-            elif colisao_com_jogador_y and not colisao_com_jogador_x:
-                # Fim de jogo
-                texto_principal.setText(f'FIM DE JOGO\n\nVocê marcou {pontos} pontos.')
-                texto_principal.draw(janela)
-                eventos.fim_de_jogo()
-                break
-
-            sentido_bolinha_y *= -1
-
-        bolinha.move(
-            velocidade_bolinha_x * sentido_bolinha_x,
-            velocidade_bolinha_y * sentido_bolinha_y
-        )
-
-        # Colisão com blocos
-        for i in range(len(blocos_lista)):
-            if blocos_lista[i] != '':
-                lado_esquerdo_bloco = blocos_lista[i].getCenter().getX() - (50 / 2)
-                lado_direito_bloco = blocos_lista[i].getCenter().getX() + (50 / 2)
-
-                lado_superior_bloco = blocos_lista[i].getCenter().getY() - (20 / 2)
-                lado_inferior_bloco = blocos_lista[i].getCenter().getY() + (20 / 2)
-
-                colisao_com_bloco_x = lado_esquerdo_bloco <= pos_atual_bolinha.getX() <= lado_direito_bloco
-                colisao_com_bloco_y = lado_superior_bloco <= pos_atual_bolinha.getY() <= lado_inferior_bloco
-
-                if colisao_com_bloco_x and colisao_com_bloco_y:
-                    blocos_lista[i].undraw()
-                    blocos_lista[i] = ''
-                    if blocos_lista[i] == '':
-                        # Colisão com bloco
-                        pontos += 1
-                        sentido_bolinha_y *= -1
-                        eventos.colisao_bola_bloco(janela, blocos_lista[i])
-
-        # Movimento do jogador
-        tecla = janela.checkKey()
-
-        if tecla == 'Left':
-            # Esquerda
-            if lado_esquerdo_jogador > 0:
-                jogador.move(- velocidade_jogador, 0)
-
-        elif tecla == 'Right':
-            # Direita
-            if lado_direito_jogador < janela.getWidth():
-                jogador.move(velocidade_jogador, 0)
-
-        # Loop principal
-        texto_pontos.setText(f'Pontos: {pontos}')
-        eventos.loop_principal()
-
-        # Jogador venceu
-        if blocos_lista.count('') == len(blocos_lista) and blocos_lista:
-            texto_principal.setText(f'Parabéns {nome}, você venceu! :)\nPontos: {pontos}')
-            texto_principal.draw(janela)
-            eventos.jogador_venceu()
-            break
-
-        update(velocidade_jogo)
-
-    janela.getMouse()
-    janela.close()
+    escolha = menu.menu(janela)
+    if escolha == 1:
+        nome = jogador.tela_nome(janela, centro_tela, cor_principal, cor_secundaria)
+        base.jogo_base(janela, centro_tela, cor_principal, nome)
+    else:
+        texto = Text(Point(centro_tela.getX(), centro_tela.getY()), 'Não implementado')
+        texto.setSize(24)
+        texto.setTextColor(cor_principal)
+        texto.draw(janela)
+        janela.getMouse()
+        janela.close()
 
 if __name__ == '__main__':
     main()
--- menu.py
+++ menu.py
@@ -1,127 +1,144 @@
 from graphics import *
-from graphics import GraphWin
 
-win = GraphWin('ball word', 800, 600)
+def menu(janela):
+    lista_objetos = []
 
-escolha = 1
-escmax = 4
+    escolha = 1
+    escmax = 4
 
-nome = Text(Point(400, 100), "Ball Word")
-nome.setFill("red")
-nome.setSize(36)
-nome.draw(win)
+    nome = Text(Point(400, 100), "Ball Word")
+    nome.setFill("red")
+    nome.setSize(36)
+    nome.draw(janela)
+    lista_objetos.append(nome)
 
-x = 100
-y = 200
+    x = 100
+    y = 200
 
-blocos = Rectangle(Point(x, y), Point(x + 200, y + 100))
-blocos.setFill("black")
-blocos.draw(win)
+    blocos = Rectangle(Point(x, y), Point(x + 200, y + 100))
+    blocos.setFill("black")
+    blocos.draw(janela)
+    lista_objetos.append(blocos)
 
-vspc = Rectangle(Point(x, y + 200), Point(x + 200, y + 300))
-vspc.setFill("black")
-vspc.draw(win)
+    vspc = Rectangle(Point(x, y + 200), Point(x + 200, y + 300))
+    vspc.setFill("black")
+    vspc.draw(janela)
+    lista_objetos.append(vspc)
 
-x = 500
+    x = 500
 
-vsps = Rectangle(Point(x, y), Point(x + 200, y + 100))
-vsps.setFill("black")
-vsps.draw(win)
+    vsps = Rectangle(Point(x, y), Point(x + 200, y + 100))
+    vsps.setFill("black")
+    vsps.draw(janela)
+    lista_objetos.append(vsps)
 
-rank = Rectangle(Point(x, y + 200), Point(x + 200, y + 300))
-rank.setFill("black")
-rank.draw(win)
+    rank = Rectangle(Point(x, y + 200), Point(x + 200, y + 300))
+    rank.setFill("black")
+    rank.draw(janela)
+    lista_objetos.append(rank)
 
-text = Text(blocos.getCenter(), "Blocos")
-text.setSize(15)
-text.setFill("green")
-text.draw(win)
+    text1 = Text(blocos.getCenter(), "Blocos")
+    text1.setSize(15)
+    text1.setFill("green")
+    text1.draw(janela)
+    lista_objetos.append(text1)
 
-text = Text(vspc.getCenter(), "VS BOT")
-text.setSize(15)
-text.setFill("green")
-text.draw(win)
+    text2 = Text(vspc.getCenter(), "VS BOT")
+    text2.setSize(15)
+    text2.setFill("green")
+    text2.draw(janela)
+    lista_objetos.append(text2)
 
-text = Text(vsps.getCenter(), "1 VS 1")
-text.setSize(15)
-text.setFill("green")
-text.draw(win)
+    text3 = Text(vsps.getCenter(), "1 VS 1")
+    text3.setSize(15)
+    text3.setFill("green")
+    text3.draw(janela)
+    lista_objetos.append(text3)
 
-op2text = Text(rank.getCenter(), "Placar")
-op2text.setSize(15)
-op2text.setFill("green")
-op2text.draw(win)
+    op2text = Text(rank.getCenter(), "Placar")
+    op2text.setSize(15)
+    op2text.setFill("green")
+    op2text.draw(janela)
+    lista_objetos.append(op2text)
 
-blocosSele = Rectangle(blocos.getP1(), blocos.getP2())
-blocosSele.setWidth(8)
-blocosSele.setOutline("red")
+    blocosSele = Rectangle(blocos.getP1(), blocos.getP2())
+    blocosSele.setWidth(8)
+    blocosSele.setOutline("red")
+    lista_objetos.append(blocosSele)
 
-vspcSele = Rectangle(vspc.getP1(), vspc.getP2())
-vspcSele.setWidth(8)
-vspcSele.setOutline("red")
+    vspcSele = Rectangle(vspc.getP1(), vspc.getP2())
+    vspcSele.setWidth(8)
+    vspcSele.setOutline("red")
+    lista_objetos.append(vspcSele)
 
-vspsSele = Rectangle(vsps.getP1(), vsps.getP2())
-vspsSele.setWidth(8)
-vspsSele.setOutline("red")
+    vspsSele = Rectangle(vsps.getP1(), vsps.getP2())
+    vspsSele.setWidth(8)
+    vspsSele.setOutline("red")
+    lista_objetos.append(vspsSele)
 
-rankSele = Rectangle(rank.getP1(), rank.getP2())
-rankSele.setWidth(8)
-rankSele.setOutline("red")
+    rankSele = Rectangle(rank.getP1(), rank.getP2())
+    rankSele.setWidth(8)
+    rankSele.setOutline("red")
+    lista_objetos.append(rankSele)
 
-while True:
-    teclas = win.checkKey()
+    while True:
+        teclas = janela.checkKey()
 
-    if teclas == "Down":
-        if escolha == escmax:
-            escolha = 1
-        else:
-            escolha += 1
-    if teclas == "Up":
+        if teclas == "Down":
+            if escolha == escmax:
+                escolha = 1
+            else:
+                escolha += 1
+        if teclas == "Up":
+            if escolha == 1:
+                escolha = escmax
+            else:
+                escolha -= 1
+
         if escolha == 1:
-            escolha = escmax
-        else:
-            escolha -= 1
+            rankSele.undraw()
+            vspcSele.undraw()
+            blocosSele.undraw()
+            blocosSele.draw(janela)
 
-    if escolha == 1:
-        rankSele.undraw()
-        vspcSele.undraw()
-        blocosSele.undraw()
-        blocosSele.draw(win)
-
-    if escolha == 2:
-        vspsSele.undraw()
-        blocosSele.undraw()
-        vspcSele.undraw()
-        vspcSele.draw(win)
-
-    if escolha == 3:
-        rankSele.undraw()
-        vspcSele.undraw()
-        vspsSele.undraw()
-        vspsSele.draw(win)
-
-    if escolha == 4:
-        vspsSele.undraw()
-        blocosSele.undraw()
-        rankSele.undraw()
-        rankSele.draw(win)
-
-    if teclas == "Return":
-        if escolha == 1:
-            print("1")
-            break
         if escolha == 2:
-            print("2")
-            break
+            vspsSele.undraw()
+            blocosSele.undraw()
+            vspcSele.undraw()
+            vspcSele.draw(janela)
+
         if escolha == 3:
-            print("3")
-            break
+            rankSele.undraw()
+            vspcSele.undraw()
+            vspsSele.undraw()
+            vspsSele.draw(janela)
+
         if escolha == 4:
-            print("4")
+            vspsSele.undraw()
+            blocosSele.undraw()
+            rankSele.undraw()
+            rankSele.draw(janela)
+
+        if teclas == "Return":
+            if escolha == 1:
+                print("1")
+                break
+            if escolha == 2:
+                print("2")
+                break
+            if escolha == 3:
+                print("3")
+                break
+            if escolha == 4:
+                print("4")
+                break
+
+        if teclas == "Escape":
             break
 
-    if teclas == "Escape":
-        break
-    update(30)
+        update(60)
 
-win.close()
+    for objeto in lista_objetos:
+        objeto.undraw()
+
+    return escolha
