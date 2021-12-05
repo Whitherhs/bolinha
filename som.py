8a0fdff03cec2e9322aea70e6d6188e00f43371f
lnrdhffmnn <90478165+lnrdhffmnn@users.noreply.github.com>
2021-12-05 20:40:18 -0300

    Alteração no áudio

--- som.py
+++ som.py
@@ -1,8 +1,11 @@
-import winsound
+import pygame
+
+pygame.mixer.init()
 
 def som_menu():
-    winsound.PlaySound('./wav/ping_pong_8bit_plop.wav', winsound.SND_ASYNC)
+    pygame.mixer.music.load('./mp3/ping_pong_8bit_beeep.mp3')
+    pygame.mixer.music.play()
 
 def som_colisao():
-    winsound.PlaySound('./wav/ping_pong_8bit_beeep.wav', winsound.SND_ASYNC)
-    pass
+    pygame.mixer.music.load('./mp3/ping_pong_8bit_beeep.mp3')
+    pygame.mixer.music.play()
